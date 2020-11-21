2a8033db08e70a97d70a1cd26062803cbcc99cdf
Stanislav Dakov <st.dakov@gmail.com>
2020-11-21 06:14:41 +0200

    update the readme file

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # bgpost
+
+This api uses screen scraping to get the package travelling information from the bulgarian post.
+
+## Guidelines
+
+### RESTful URLs
+
+- Get package data:
+  - GET https://bgpost.dakovdev.com/api/v1/?code=123
--- public/index.php
+++ public/index.php
@@ -12,13 +12,13 @@ function dd($a)
     echo "<pre>";
     print_r($a);
 }
-if (!array_key_exists("code", $_POST) || trim($_POST['code']) == "") {
+if (!array_key_exists("code", $_GET) || trim($_GET['code']) == "") {
     http_response_code(404);
     die();
 }
 $bgpost = new \Tracking\BgPostService();
 
-$tableData = $bgpost->track(trim($_POST['code']));
+$tableData = $bgpost->track(trim($_GET['code']));
 if (count($tableData) == 1 && $tableData[0]["status"] == \Tracking\BgPostService::EVENT_STATUS_WRONG_CODE) {
     http_response_code(404);
 }
