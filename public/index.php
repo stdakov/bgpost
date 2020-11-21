b75943461b7b685eb0cbbb4c6bdd1440fda5b4cd
Stanislav Dakov <st.dakov@gmail.com>
2020-11-21 06:19:24 +0200

    update the api

--- public/index.php
+++ public/index.php
@@ -12,15 +12,16 @@ function dd($a)
     echo "<pre>";
     print_r($a);
 }
+$tableData = [];
 if (!array_key_exists("code", $_GET) || trim($_GET['code']) == "") {
     http_response_code(404);
-    die();
-}
-$bgpost = new \Tracking\BgPostService();
+} else {
+    $bgpost = new \Tracking\BgPostService();
 
-$tableData = $bgpost->track(trim($_GET['code']));
-if (count($tableData) == 1 && $tableData[0]["status"] == \Tracking\BgPostService::EVENT_STATUS_WRONG_CODE) {
-    http_response_code(404);
+    $tableData = $bgpost->track(trim($_GET['code']));
+    if (count($tableData) == 1 && $tableData[0]["status"] == \Tracking\BgPostService::EVENT_STATUS_WRONG_CODE) {
+        http_response_code(404);
+    }
 }
 
 echo json_encode($tableData);
