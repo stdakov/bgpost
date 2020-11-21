913a69b7c31df53c8454849a3fb36133ed42e208
Stanislav Dakov <st.dakov@gmail.com>
2020-11-21 05:09:57 +0200

    init

---
+++ public/index.php
@@ -0,0 +1,25 @@
+<?php
+require_once "../vendor/autoload.php";
+
+header('Content-Type: application/json; charset=utf-8');
+
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
+//error_reporting(E_ALL);
+
+function dd($a)
+{
+    echo "<pre>";
+    print_r($a);
+}
+if (!array_key_exists("code", $_POST)) {
+    die();
+}
+$bgpost = new \Tracking\BgPostService();
+
+$tableData = $bgpost->track($_POST['code']);
+if (count($tableData) == 1 && $tableData[0]["status"] == \Tracking\BgPostService::EVENT_STATUS_WRONG_CODE) {
+    http_response_code(404);
+}
+
+echo json_encode($tableData);
---
+++ src/BgPostService.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace Tracking;
+
+use FastSimpleHTMLDom\Document;
+
+class BgPostService
+{
+    protected $bgPostUrl = "http://www.bgpost.bg/IPSWebTracking/IPSWeb_item_events.asp?itemid=";
+    /** @var  Document */
+    protected $dom;
+
+    const DATE_LABEL = "Local Date and Time";
+    const COUNTRY_LABEL = "Country";
+    const LOCATION_LABEL = "Location";
+    const EVENT_LABEL = "Event Type";
+    const INFO_LABEL = "Extra Information";
+
+    const DATE_JSON_LABEL = "date";
+    const COUNTRY_JSON_LABEL = "country";
+    const LOCATION_JSON_LABEL = "location";
+    const EVENT_JSON_LABEL = "event";
+    const STATUS_JSON_LABEL = "status";
+    const INFO_JSON_LABEL = "info";
+
+    const EVENT_STATUS_TRAVELING = "traveling";
+    const EVENT_STATUS_ARRIVED = "arrived";
+    const EVENT_STATUS_WRONG_CODE = "wrong_code";
+
+    public function __construct()
+    {
+    }
+
+    /**
+     * @param string $trackingNumber
+     * @return array
+     */
+    public function track(string $trackingNumber): array
+    {
+        $this->dom = Document::file_get_html($this->bgPostUrl . $trackingNumber);
+
+        $tableBody = $this->dom->find('td.tabproperty')->find("tbody", 0);
+        $tableData = [];
+
+        if ($tableBody != null) {
+            $trs = $tableBody->find("tr")->getArrayCopy();
+
+            $postInfo = array_shift($trs);
+            $headers = array_shift($trs)->find("td")->getArrayCopy();
+
+            foreach ($trs as $tr) {
+                $trData = [];
+                $tds = $tr->find("td");
+                foreach ($tds as $key => $td) {
+                    if (!array_key_exists($key, $headers)) {
+                        continue;
+                    }
+                    $trData[$this->getIndexName(trim($headers[$key]->plaintext))] = trim($td->plaintext);
+                }
+
+                $trData[self::STATUS_JSON_LABEL] = $this->getStatus($trData[self::EVENT_JSON_LABEL]);
+
+                $tableData[] = $trData;
+            }
+        }
+
+        return $tableData;
+    }
+
+    public function getIndexName(string $tableLabel): string
+    {
+        $labelIndex = "";
+        switch ($tableLabel) {
+            case self::DATE_LABEL:
+                $labelIndex = self::DATE_JSON_LABEL;
+                break;
+            case self::COUNTRY_LABEL:
+                $labelIndex = self::COUNTRY_JSON_LABEL;
+                break;
+            case self::LOCATION_LABEL:
+                $labelIndex = self::LOCATION_JSON_LABEL;
+                break;
+            case self::EVENT_LABEL:
+                $labelIndex = self::EVENT_JSON_LABEL;
+                break;
+            case self::INFO_LABEL:
+                $labelIndex = self::INFO_JSON_LABEL;
+                break;
+        }
+
+        return $labelIndex;
+    }
+
+    public function getStatus(string $eventType): string
+    {
+        $status = "traveling";
+
+        if (trim($eventType) == "Пратката  e получена") {
+            $status = "arrived";
+        }
+
+        if (trim($eventType) == "Грешен код на пратката") {
+            $status = "wrong_code";
+        }
+
+        return $status;
+    }
+}
