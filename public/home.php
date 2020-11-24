b29f1299c636b4dadaa2aca86f5742e20816a25b
Stanislav Dakov <st.dakov@gmail.com>
2020-11-24 16:38:54 +0200

    added google counter

--- public/home.php
+++ public/home.php
@@ -48,6 +48,20 @@
     <meta name="viewport" content="width=device-width" />
     <meta name="description" content="Проследяване на пратки по Български пощи, пратки от aliexpress, пратка по пощата, bgpost" />
 
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-VQJ5L4HHEW"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-VQJ5L4HHEW');
+    </script>
+
 </head>
 
 <body>
